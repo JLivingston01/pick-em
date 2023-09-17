9b6ade562723310ef690dc62bcf08501e3511174
JLivingston01 <jason.livingston@caa.columbia.edu>
2023-09-17 17:42:57 -0400

    network changes

--- scripts/data_preparation.py
+++ scripts/data_preparation.py
@@ -1,90 +1,118 @@
 
 import pandas as pd
 import numpy as np
+import os
 
-
-def make_merge_frame(target_season_num:int,games:pd.DataFrame) -> pd.DataFrame:
+def make_merge_frame(games:pd.DataFrame) -> pd.DataFrame:
     """Merges player stats from season t-1 onto rosters in season t, aggregates
     to the game level (home and away stats) with game-level scores and the spread.
 
     Args:
-        target_season_num (int): year of the target season
         games (DataFrame): df of games, sliced to the target season
 
     Returns:
         DataFrame: Game data for season t with aggregated player stats from season t-1
     """
-    rosters = pd.read_parquet(f"data/roster_{target_season_num}.parquet")
-    stats = pd.read_parquet(f"data/player_stats_{target_season_num-1}.parquet")
-    rosters['player_display_name'] = rosters['full_name'] 
-    target_season = games[games['season']==target_season_num].copy()#[(games['season']==2023)&(games['week']==1)]
+    all_data = os.listdir("data/")
+    roster_list = [i for i in all_data if 'roster_' in i]
+    stats_list = [i for i in all_data if 'player_stats_' in i]
 
-    agg_stats = stats[stats['season_type']=='REG'].groupby(['player_display_name']).agg({
-        'completions':'mean',
-        'attempts':'mean',
-        'passing_yards':'mean',
-        'passing_tds':'mean',
-        'interceptions':'mean',
-        'sacks':'mean',
-        'sack_yards':'mean',
-        'sack_fumbles':'mean',
-        'sack_fumbles_lost':'mean',
-        'passing_air_yards':'mean',
-        'passing_yards_after_catch':'mean',
-        'passing_first_downs':'mean',
-        'passing_epa':'mean',
-        'passing_2pt_conversions':'mean',
-        'pacr':'mean',
-        'carries':'mean',
-        'rushing_yards':'mean',
-        'rushing_tds':'mean',
-        'rushing_fumbles':'mean',
-        'rushing_fumbles_lost':'mean',
-        'rushing_first_downs':'mean',
-        'rushing_epa':'mean',
-        'rushing_2pt_conversions':'mean',
-        'receptions':'mean',
-        'targets':'mean',
-        'receiving_yards':'mean',
-        'receiving_tds':'mean',
-        'receiving_fumbles':'mean',
-        'receiving_fumbles_lost':'mean',
-        'receiving_air_yards':'mean',
-        'receiving_yards_after_catch':'mean',
-        'receiving_first_downs':'mean',
-        'receiving_epa':'mean',
-        'receiving_2pt_conversions':'mean',
-        'racr':'mean',
-        'target_share':'mean',
-        'air_yards_share':'mean',
-        'wopr':'mean',
-        'special_teams_tds':'mean',
-        'fantasy_points':'mean',
-        'fantasy_points_ppr':'mean',
-        'week':'count'
-    }).reset_index()
+    all_rosters = pd.DataFrame()
+    all_stats = pd.DataFrame()
+
+    for filename in roster_list:
+        tmp = pd.read_parquet(f"data/{filename}")
+        all_rosters = pd.concat([all_rosters,tmp])
+
+    for filename in stats_list:
+        tmp = pd.read_parquet(f"data/{filename}")
+        all_stats = pd.concat([all_stats,tmp])
+
+    all_stats['merge_season'] = all_stats['season']+1
+
+    all_rosters['player_display_name'] = all_rosters['full_name'] 
+    all_rosters['merge_season'] = all_rosters['season']
+
+    agg_stats = all_stats[all_stats['season_type']=='REG'].groupby(
+        ['player_display_name','merge_season']).agg({
+            'completions':'mean',
+            'attempts':'mean',
+            'passing_yards':'mean',
+            'passing_tds':'mean',
+            'interceptions':'mean',
+            'sacks':'mean',
+            'sack_yards':'mean',
+            'sack_fumbles':'mean',
+            'sack_fumbles_lost':'mean',
+            'passing_air_yards':'mean',
+            'passing_yards_after_catch':'mean',
+            'passing_first_downs':'mean',
+            'passing_epa':'mean',
+            'passing_2pt_conversions':'mean',
+            'pacr':'mean',
+            'carries':'mean',
+            'rushing_yards':'mean',
+            'rushing_tds':'mean',
+            'rushing_fumbles':'mean',
+            'rushing_fumbles_lost':'mean',
+            'rushing_first_downs':'mean',
+            'rushing_epa':'mean',
+            'rushing_2pt_conversions':'mean',
+            'receptions':'mean',
+            'targets':'mean',
+            'receiving_yards':'mean',
+            'receiving_tds':'mean',
+            'receiving_fumbles':'mean',
+            'receiving_fumbles_lost':'mean',
+            'receiving_air_yards':'mean',
+            'receiving_yards_after_catch':'mean',
+            'receiving_first_downs':'mean',
+            'receiving_epa':'mean',
+            'receiving_2pt_conversions':'mean',
+            'racr':'mean',
+            'target_share':'mean',
+            'air_yards_share':'mean',
+            'wopr':'mean',
+            'special_teams_tds':'mean',
+            'fantasy_points':'mean',
+            'fantasy_points_ppr':'mean',
+            'week':'count'
+        }).reset_index()
+
     agg_stats.rename({'week':'games'},axis=1,inplace=True)
     agg_stats = pd.DataFrame(np.where(agg_stats==0,np.nan,agg_stats),columns=agg_stats.columns)
-    roster_stats = rosters.merge(agg_stats,on=['player_display_name'],how='inner')
-    team_stats = roster_stats.groupby(['team']).agg({
-        i:'sum' for i in agg_stats.columns if i not in ['player_display_name']
-    }).reset_index()
-    prep_frame = target_season[['week','away_team','home_team','away_score','home_score','result','spread_line']+
-                               [i for i in target_season.columns if ('roof_' in i)|('surface_' in i)|('away_team_id' in i)|('home_team_id' in i)]].copy()
+
+    roster_stats = all_rosters.merge(agg_stats,on=['player_display_name','merge_season'],how='inner')
+
+    team_stats = roster_stats.groupby(['team','season']).agg({
+            i:'sum' for i in agg_stats.columns if i not in ['player_display_name','merge_season']
+        }).reset_index()
+
+    team_stats.to_csv("data/seasonal_team_stats.csv",index=False)
+
+    prep_frame = games[['season','week','away_team','home_team','away_score','home_score','result','spread_line']+
+                    [i for i in games.columns if ('roof_' in i)|('surface_' in i)|('away_team_id' in i)|('home_team_id' in i)]].copy()
+
     away_team_stats = team_stats.copy()
     home_team_stats = team_stats.copy()
 
     away_team_stats.columns = ['away_'+i for i in team_stats.columns]
     home_team_stats.columns = ['home_'+i for i in team_stats.columns]
 
-    merge = prep_frame.merge(away_team_stats,on='away_team',how='left').merge(home_team_stats,on='home_team',how='left')
+    home_team_stats['season'] = home_team_stats['home_season']
+    away_team_stats['season'] = away_team_stats['away_season']
 
-    merge['season'] = target_season_num
+    home_team_stats.drop(["home_season"],axis=1,inplace=True)
+    away_team_stats.drop(["away_season"],axis=1,inplace=True)
+
+    merge = prep_frame.merge(away_team_stats,on=['away_team','season'],how='left').merge(home_team_stats,on=['home_team','season'],how='left')
+
     return merge
 
 def main() -> None:
 
     games = pd.read_csv('data/games.csv')
+    pd.set_option("display.max_columns",55)
 
     games['surface']=games['surface'].str.replace(" ","")
     games['roof']=games['roof'].str.replace(" ","")
@@ -100,14 +128,15 @@ def main() -> None:
     away_roof = pd.get_dummies(games['away_roof'],drop_first=True,dtype=int,prefix='away_roof')
     away_surface = pd.get_dummies(games['away_surface'],drop_first=True,dtype=int,prefix='away_surface')
 
+
     games[roof.columns]=roof
     games[surface.columns]=surface
     games[away_roof.columns]=away_roof
     games[away_surface.columns]=away_surface
 
-    merges = [make_merge_frame(target_season_num=i,games=games) for i in [2023,2022,2021,2020,2019,2018]]
+    games = games[games['season']>=2018].copy()
 
-    all_dat = pd.concat(merges)
+    all_dat = make_merge_frame(games=games)
 
     all_dat.to_csv("data/model_data.csv",index=False)
 
